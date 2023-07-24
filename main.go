f579d8e538f2ef91c7f07f8f740469a65a173721
Anwesh43 <anweshthecool0@gmail.com>
2023-07-25 01:40:22 +0530

    adding cors changes

--- main.go
+++ main.go
@@ -3,13 +3,27 @@ package main
 import (
 	"log"
 
+	"time"
+
 	"com.loan.demo/helpers"
 	"com.loan.demo/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	helpers.LoadEnv()
 	db, err := helpers.ConnectToDb()
 	routeHandler := routes.NewRouteHandler(db)
